ed5db392cb5a52db386856eab379c4dc82faaaad
RaushanSagar1694 <dayasagar1694@gmail.com>
2024-09-22 16:15:51 +0530

    add conversion file

---
+++ JavaScript/conversionOperations.js
@@ -0,0 +1,17 @@
+
+
+// ******************* OPERATIONS ******************** //
+
+
+// console.log(2+2);
+// console.log(3-2);
+// console.log(4*2);
+// console.log(4/2);
+// console.log(2**3);
+// console.log(21%4);
+
+
+let val = "12" + 24
+
+console.log(+"hf")
+
